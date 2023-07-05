f264c6d3971cdbd03810e4828e1e9b9f4c068961
Ramin Haghjou <haghjouramin@gmail.com>
2023-07-05 16:34:22 +0330

    add restFull api

--- app/presentation/rest_api/cities/CityProvinceAPI.py
+++ app/presentation/rest_api/cities/CityProvinceAPI.py
@@ -1,28 +1,36 @@
 import jsonpickle
 
 from flask import Blueprint, request
-from oauthlib.oauth2 import BackendApplicationClient
-from requests_oauthlib import OAuth2Session
+from flask_restful import Resource, Api
 from app.core.services.cities.CityProvinceAppService import CityProvinceAPPService
 from app.infrastructure.presenters.FileCityProvinceRepository import FileCityProvinceRepository
 
-city_blueprint = Blueprint('city', __name__)
+class AddCityProvinceAPI(Resource):
+    def post(self):
+        city = request.form['city']
+        province = request.form['province']
+
+        city_province = CityProvinceAPPService(FileCityProvinceRepository())
+        a = city_province.add(city, province)
+
+        return str(a)
 
 
-@city_blueprint.route('/add-city', methods=["POST"])
-def save_city_province():
-    city = request.form['city']
-    province = request.form['province']
+class City(Resource):
+    def get(self):
+        city_province_id = request.args.get('id')
 
-    city_province = CityProvinceAPPService(FileCityProvinceRepository())
-    a = city_province.add(city, province)
+        city_province = CityProvinceAPPService(FileCityProvinceRepository())
+        return city_province.get(city_province_id)
 
-    return str(a)
+    def delete(self):
+        city_province_id = request.args.get('id')
+
+        city_province = CityProvinceAPPService(FileCityProvinceRepository())
+        return city_province.delete(city_province_id)
 
 
-@city_blueprint.route('/get-city', methods=['GET'])
-def get_city_province():
-    city_province_id = request.args.get('id')
-
-    city_province = CityProvinceAPPService(FileCityProvinceRepository())
-    return city_province.get(city_province_id)
+city_api = Blueprint('rest_api/cities/city', __name__)
+api = Api(city_api)
+api.add_resource(AddCityProvinceAPI, '/cities', endpoint='cities')
+api.add_resource(City, '/cities/<int:id>', endpoint='city')
--- app/presentation/rest_api/config/RoutesExtension.py
+++ app/presentation/rest_api/config/RoutesExtension.py
@@ -1,7 +1,7 @@
 # app/extensions
-from app.presentation.rest_api.cities.CityProvinceAPI import city_blueprint
+from app.presentation.rest_api.cities.CityProvinceAPI import city_api
 
 
 def register_routes(app):
     """ Register routes with blueprint and namespace """
-    app.register_blueprint(city_blueprint)
+    app.register_blueprint(city_api, url_prefix='/api/v1/')
